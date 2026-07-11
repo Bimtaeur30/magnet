84041bce7047a9e0b16f14cbfd103c3abe9ee19f
common0902 <minsoung200@gmail.com>
2026-07-11 11:55:38 +0900

    feat : Skin Code

---
+++ magnet/Assets/_MemberWorkspace/PMS/Scripts/Skin/SkinDataSO.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+namespace PMS.Scripts.Skin
+{
+    [CreateAssetMenu(fileName = "Skin data", menuName = "Skin/SkinData")]
+    public class SkinDataSO : ScriptableObject
+    {
+        public string skinId;
+        public string skinName;
+
+        public Sprite icon;
+        public Sprite blockCellSprite;
+
+        public SkinUnlockTypeEnum unlockType;
+        public int unlockValue;
+        public string unlockDescription;
+    }
+}
