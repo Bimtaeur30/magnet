dcf9056eddd27b1672139a5a8df44dca27db68bd
common0902 <minsoung200@gmail.com>
2026-07-13 23:38:08 +0900

    feat : SkinDataListSO

---
+++ magnet/Assets/_MemberWorkspace/PMS/Scripts/Skin/SkinDataListSO.cs
@@ -0,0 +1,12 @@
+﻿using PMS.Scripts.Skin;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace PMS.Scripts.Skin
+{
+    [CreateAssetMenu(fileName = "Skin data list", menuName = "Skin/SkinDataList")]
+    public class SkinDataListSO : ScriptableObject
+    {
+        [field: SerializeField] public List<SkinDataSO> Skins {  get; private set; }
+    }
+}
